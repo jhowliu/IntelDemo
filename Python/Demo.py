977507a6b3212e2c30ade06695fdd4fbcddda9da
Tomlong <boy82530ten@gmail.com>
2014-12-26 16:49:04 +0800

    adjust Demo.py

--- Python/Demo.py
+++ Python/Demo.py
@@ -2,7 +2,8 @@
 import serial
 import os.path
 import sys
-from PreProcessing import PreProcessing 
+import numpy as np
+from PreProcessing import Preprocessing 
 from Vectorization import Vectorize
 from datetime import datetime
 
@@ -10,18 +11,18 @@ def OpenSerial():
     return serial.Serial('/dev/ttyACM0', 9600)
 
 def writeInFile(name, data):
-    fileName = name + ".csv"
+    fileName = name  
     currentTime = datetime.now()
 
-    with open(fileName, 'a') as f:
+    with open(fileName, 'w') as f:
         for tmp in data:
             f.write(tmp + "," + str(currentTime.hour) + "," + str(currentTime.weekday()+1) +  "\n")
 def Run(name):
     # Load the data in numpy's type
-    data = np.loadtxt(name, delimiter=',')
+    data = np.genfromtxt(name, delimiter=',')
     # Do preprocessing & moving average
-    [axis1, axis2, axis3, axis4, axis5, axis6, press1, press2, press3, press4] = PreProcessing(data)
-    preproData = [axis1, axis2, axis3, axis4, axis5, axis6, press1, press2, press3, press4]
+    [axis1, axis2, axis3, axis4, axis5, axis6, press1, press2, press3, press4] = Preprocessing(data)
+    preproData = np.array([axis1, axis2, axis3, axis4, axis5, axis6, press1, press2, press3, press4])
     # Vectorization
     vectorFeature = Vectorize(preproData)
     print "Finish"
@@ -43,10 +44,8 @@ def Read(name):
 
         if (line == "Closed"):
             writeInFile(name, dataList)
-            print count
             # Run the Main function
             Run(name)
-            count +=1
             dataList = []
 
         line = ser.readline()
